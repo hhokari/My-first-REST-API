740c784148559be706adfd91c19f5d8e18cfcd01
hhokari <hhokari@calpoly.edu>
2021-01-13 10:36:05 -0800

    Deleted comments

--- sample_backend.py
+++ sample_backend.py
@@ -3,10 +3,6 @@ from flask import request
 from flask import jsonify
 app = Flask(__name__)
 
-#@app.route('/')
-#def hello_world():
-#   return 'Hello, World!'
-
 users = { 
    'users_list' :
    [
@@ -37,30 +33,6 @@ users = {
       }
    ]
 }
-
-#@app.route('/users')
-#def get_users():
-#   return users
-
-#@app.route('/users')
-#def get_users():
-#   search_username = request.args.get('name') #accessing the value of parameter 'name'
-#   if search_username :
-#      subdict = {'users_list' : []}
-#      for user in users['users_list']:
-#         if user['name'] == search_username:
-#            subdict['users_list'].append(user)
-#      return subdict
-#   return users
-
-#@app.route('/users/<id>')
-#def get_user(id):
-#   if id :
-#      for user in users['users_list']:
-#        if user['id'] == id:
-#           return user
-#      return ({})
-#   return users
 
 @app.route('/users', methods=['GET', 'POST'])
 def get_users():
